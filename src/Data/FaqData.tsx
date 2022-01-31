0ec4bd84987c848662116c7715103f80ef121c2f
ernest <ernest_everstov@mail.ru>
2022-01-31 20:13:13 +0900

    delete question to faq

--- src/Data/FaqData.tsx
+++ src/Data/FaqData.tsx
@@ -1,34 +1,34 @@
 import React from "react";
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage } from "react-intl";
 
 const Data = [
     {
-        question: <FormattedMessage id={'faq_screening_canser'}/>,
-        answer: <FormattedMessage id={'faq_answer_screening_canser'}/>
+        question: <FormattedMessage id={'faq_screening_canser'} />,
+        answer: <FormattedMessage id={'faq_answer_screening_canser'} />
     },
     {
-        question: <FormattedMessage id={'faq_early_diagnosis'}/>,
-        answer: <FormattedMessage id={'faq_answer_early_diagnosis'}/>
+        question: <FormattedMessage id={'faq_early_diagnosis'} />,
+        answer: <FormattedMessage id={'faq_answer_early_diagnosis'} />
     },
     {
-        question: <FormattedMessage id={'faq_how_does'}/>,
-        answer: <FormattedMessage id={'faq_answer_how_does'}/>
+        question: <FormattedMessage id={'faq_how_does'} />,
+        answer: <FormattedMessage id={'faq_answer_how_does'} />
     },
     {
-        question: <FormattedMessage id={'faq_not_working_surveys'}/>,
-        answer: <FormattedMessage id={'faq_answer_not_working_surveys'}/>
+        question: <FormattedMessage id={'faq_not_working_surveys'} />,
+        answer: <FormattedMessage id={'faq_answer_not_working_surveys'} />
     },
     {
-        question: <FormattedMessage id={'faq_i_have_cancer'}/>,
-        answer: <FormattedMessage id={'faq_answer_i_have_cancer'}/>
+        question: <FormattedMessage id={'faq_i_have_cancer'} />,
+        answer: <FormattedMessage id={'faq_answer_i_have_cancer'} />
     },
+    // {
+    //     question: 'В какой форме проходит консультация у психолога?',
+    //     answer: 'Консультации могут проходить непосредственно в психологическом кабинете в Центре социально-психологической поддержки семьи и молодежи по адресу: г. Якутск, ул. Каландарашвили 21. Также консультации могут быть в онлайн формате через видеозвононк по мобильному телефону, Skype или Zoom.'
+    // },
     {
-        question: 'В какой форме проходит консультация у психолога?',
-        answer: 'Консультации могут проходить непосредственно в психологическом кабинете в Центре социально-психологической поддержки семьи и молодежи по адресу: г. Якутск, ул. Каландарашвили 21. Также консультации могут быть в онлайн формате через видеозвононк по мобильному телефону, Skype или Zoom.'
-    },
-    {
-        question: <FormattedMessage id={'faq_why_pilot_project'}/>,
-        answer: <FormattedMessage id={'faq_answer_why_pilot_project'}/>
+        question: <FormattedMessage id={'faq_why_pilot_project'} />,
+        answer: <FormattedMessage id={'faq_answer_why_pilot_project'} />
     }
 ]
 
