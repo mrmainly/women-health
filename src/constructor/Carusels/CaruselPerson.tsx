6c1a3e37d263f1107fd4adee71e353e3a449e5fb
ernest <ernest_everstov@mail.ru>
2022-02-03 21:12:20 +0900

    change arrows to peron carusel

--- src/constructor/Carusels/CaruselPerson.tsx
+++ src/constructor/Carusels/CaruselPerson.tsx
@@ -1,10 +1,66 @@
 import React from 'react'
 
-import { Box } from '@mui/material'
+import { Box, MenuItem } from '@mui/material'
+import { styled } from '@mui/system'
 import Slider from 'react-slick'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 import CardPersonData from '../../Data/CardPerson'
-import { CardPerson, ArrowPrev, ArrowNext } from '../../components'
+import { CardPerson } from '../../components'
+import { ArrowPrevProps } from '../../interface';
+import { ArrowNextProps } from '../../interface';
+
+const ArrowNextItem = styled(Box)(({ theme }) => ({
+    height: 60,
+    width: 60,
+    position: "absolute",
+    top: '50%',
+    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    right: '-35px',
+    backgroundColor: '#EB5757',
+    cursor: 'pointer',
+    [theme.breakpoints.down('md')]: {
+        right: 0
+    },
+}))
+
+const ArrowPrevItem = styled(Box)(({ theme }) => ({
+    height: 60,
+    width: 60,
+    position: "absolute",
+    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    top: '50%',
+    zIndex: 900,
+    left: '-35px',
+    cursor: 'pointer',
+    backgroundColor: '#EB5757',
+    [theme.breakpoints.down('md')]: {
+        left: 0
+    },
+}))
+
+const ArrowNext: React.FC<ArrowNextProps> = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <ArrowNextItem className="slick-arrow" onClick={onClick} >
+            <ArrowForwardIcon fontSize={'large'} htmlColor={'white'} />
+        </ArrowNextItem>
+    )
+}
+
+const ArrowPrev: React.FC<ArrowPrevProps> = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <ArrowPrevItem className="slick-arrow" onClick={onClick} >
+            <ArrowBackIcon fontSize={'large'} htmlColor={'white'} />
+        </ArrowPrevItem>
+    )
+}
 
 const CaruselPerson = () => {
     const settings = {
@@ -14,8 +70,8 @@ const CaruselPerson = () => {
         speed: 500,
         slidesToScroll: 1,
         slidesToShow: 1,
-        nextArrow: <ArrowNext style={{ right: '-20px' }} />,
-        prevArrow: <ArrowPrev style={{ left: '-20px' }} />,
+        nextArrow: <ArrowNext />,
+        prevArrow: <ArrowPrev />,
     };
     return (
         <Box >
