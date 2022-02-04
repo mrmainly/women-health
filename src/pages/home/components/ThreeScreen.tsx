611a6fea48e042ccbe4a853f452756300d7beabd
ernest <ernest_everstov@mail.ru>
2022-02-04 18:30:07 +0900

    fixed companys in homepage

--- src/pages/home/components/ThreeScreen.tsx
+++ src/pages/home/components/ThreeScreen.tsx
@@ -3,19 +3,31 @@ import React from 'react'
 import { Grid, Box } from '@mui/material'
 import { styled } from '@mui/system'
 import { MyContainer, MyText } from '../../../components'
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage } from "react-intl";
 
 const Companys = styled(Grid)(({ }) => ({
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'column',
     alignItems: 'center',
-    padding: 20,
+
 }))
 
-const CompanyItem = styled(Grid)(({ }) => ({
+const CompanyItem = styled(Box)(({ theme }) => ({
     marginLeft: 15,
-    marginRight: 15
+    marginRight: 15,
+    [theme.breakpoints.down('sm')]: {
+        marginLeft: 0,
+        marginRight: 0
+    },
+}))
+
+const Box1 = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+    flexWrap: 'wrap'
 }))
 
 const ThreeScreen: React.FC = () => {
@@ -29,15 +41,15 @@ const ThreeScreen: React.FC = () => {
         >
             <Companys container>
                 <MyText variant="h5">
-                    <FormattedMessage id={'partners_project'}/>
+                    <FormattedMessage id={'partners_project'} />
                 </MyText>
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 3 }}>
+                <Box1>
                     {CompanyList.map((itemCompany) => (
-                        <CompanyItem item key={itemCompany}>
+                        <CompanyItem key={itemCompany}>
                             <img src={`/img/Element/${itemCompany}.png`} />
                         </CompanyItem>
                     ))}
-                </Box>
+                </Box1>
             </Companys>
         </MyContainer>
     )
