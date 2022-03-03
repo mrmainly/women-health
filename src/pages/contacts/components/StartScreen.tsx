5405e1e459c5bd9199348ae4dc2b98504bfdca46
ernest <ernest_everstov@mail.ru>
2022-03-03 13:38:19 +0900

    changed contacts page

--- src/pages/contacts/components/StartScreen.tsx
+++ src/pages/contacts/components/StartScreen.tsx
@@ -41,20 +41,20 @@ const StartScreen = () => {
     return (
         <MyContainer wrapper={true}
             sx={{
-                bgcolor: themeMain.palette.primary.main,
+                background: 'radial-gradient(71.59% 70.87% at 76.67% 104.96%, rgba(0, 94, 43, 0.4) 0%, rgba(0, 99, 45, 0.4) 0.01%, rgba(0, 169, 78, 0) 100%), linear-gradient(170.76deg, rgba(10, 116, 1, 0.4) -0.06%, rgba(192, 173, 0, 0.088) 95.53%), #76C38B;',
                 display: 'flex',
                 alignItems: 'center',
             }}
         >
             <Root container>
-                <Grid item lg={6} xl={6} md={6} sm={12} xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Img src={'/img/Element/Group420.png'} />
-                </Grid>
-                <GridItemText item lg={6} xl={6} md={6} sm={12} xs={12}>
-                    <MyText variant="h4" sx={{ color: 'black' }}><FormattedMessage id="contacts_reference_phones" /></MyText>
-                    <MyText variant="h6" sx={{ mt: 5, mb: 5, color: '#313131', fontWeight: 500 }}><FormattedMessage id="contacts_reference_phones_description" /></MyText>
-                    <MyText variant="h2" sx={{ color: '#1B1642', fontWeight: 500 }}>8 962 731-18-00</MyText>
+                <GridItemText item lg={6} xl={6} md={6} sm={12} xs={12} sx={{ color: 'white' }}>
+                    <MyText variant="h4" ><FormattedMessage id="contacts_reference_phones" /></MyText>
+                    <MyText variant="h6" sx={{ mt: 5, mb: 5, fontWeight: 500 }}><FormattedMessage id="contacts_reference_phones_description" /></MyText>
+                    <MyText variant="h2" sx={{ fontWeight: 500 }}>8-800-100-14-03</MyText>
                 </GridItemText>
+                <Grid item lg={6} xl={6} md={6} sm={12} xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Img src={'/img/Element/Group2435.png'} />
+                </Grid>
             </Root>
         </MyContainer>
     )
